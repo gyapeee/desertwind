a05d1b7e34b67e47fee18189fa3150ede36df0b1
gyapeee <gyapeee@gmail.com>
2022-05-17 21:05:57 +0200

    Basic DB based on hibernate and JPA

---
+++ src/main/java/com/kodenigaz/desertwind/database/UserState.java
@@ -0,0 +1,44 @@
+package com.kodenigaz.desertwind.database;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity(name = "USERSTATE")
+public class UserState {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long userId;
+
+    public UserState() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserState{" +
+                "id=" + id +
+                ", userId=" + userId +
+                '}';
+    }
+}
